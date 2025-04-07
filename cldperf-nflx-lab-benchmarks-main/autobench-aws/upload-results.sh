88a300c560bc70d2423e306eab6d0b771e638713
Khan <noorkhan@amd.com>
2025-04-07 11:27:00 -0400

    Updated upload to s3

--- cldperf-nflx-lab-benchmarks-main/autobench-aws/upload-results.sh
+++ cldperf-nflx-lab-benchmarks-main/autobench-aws/upload-results.sh
@@ -32,10 +32,31 @@ fi
 S3_DESTINATION="$S3_BUCKET/${INSTANCE_FOLDER}/${INSTANCE_FOLDER}_${PROFILE_TYPE}_results"
 echo "Will upload to: $S3_DESTINATION"
 
-aws s3 sync "$LOCAL_RESULTS_DIR" "$S3_DESTINATION" --exclude "*.tmp"
+echo "Calculating total files to upload..."
+TOTAL_FILES=$(find "$LOCAL_RESULTS_DIR" -type f | wc -l)
+echo "Uploading $TOTAL_FILES files..."
 
-if [ $? -eq 0 ]; then
-    echo "Upload successful!"
-else
-    echo "Upload failed. Check AWS credentials and permissions."
-fi
+# Create a temporary file for the list of files
+TEMP_FILE=$(mktemp)
+find "$LOCAL_RESULTS_DIR" -type f > "$TEMP_FILE"
+
+COUNTER=0
+while IFS= read -r file; do
+    COUNTER=$((COUNTER+1))
+    PERCENTAGE=$((COUNTER*100/TOTAL_FILES))
+
+    FILENAME=$(basename "$file")
+    RELATIVE_PATH=${file#"$LOCAL_RESULTS_DIR/"}
+    S3_PATH="$S3_DESTINATION/$RELATIVE_PATH"
+
+    # Clear the line and show progress
+    echo -ne "Uploading: [$PERCENTAGE%] $COUNTER of $TOTAL_FILES files\r"
+
+    # Upload the file silently
+    aws s3 cp "$file" "$S3_PATH" > /dev/null 2>&1
+done < "$TEMP_FILE"
+
+# Clean up
+rm "$TEMP_FILE"
+
+echo -e "\nUpload complete!"
