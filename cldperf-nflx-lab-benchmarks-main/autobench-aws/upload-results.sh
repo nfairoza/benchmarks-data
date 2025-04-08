91a44c597acc988fcc7e1502df30e15b76a3788c
Khan <noorkhan@amd.com>
2025-04-07 23:18:40 -0400

    added script

--- cldperf-nflx-lab-benchmarks-main/autobench-aws/upload-results.sh
+++ cldperf-nflx-lab-benchmarks-main/autobench-aws/upload-results.sh
@@ -64,13 +64,14 @@ if [ $? -eq 0 ]; then
     # Clean up the temporary zip file
     rm -f "$ZIP_PATH"
     echo "Temporary zip file removed"
-    
+
     echo "Generating updated index.html..."
     ./scripts/generate_index.sh "$S3_BUCKET"
 
     echo "Uploading updated index.html..."
     INDEX_PATH="./scripts/index.html"
-    aws s3 cp "$INDEX_PATH" "$S3_DESTINATION/index.html" --only-show-errors --no-progress
+    aws s3 cp "$INDEX_PATH" "$S3_BUCKET/index.html" --only-show-errors --no-progress
+    echo "Successfully uploaded index file to $S3_BUCKET/index.html "
 else
     echo "Upload failed. Check AWS credentials and permissions."
 fi
