3a1ac6f62ad621432c735c99e7a4ca1703d2af80
Goel <palagoel@amd.com>
2025-04-14 15:30:29 -0700

    removing baremetal script to revamp

--- cldperf-nflx-lab-benchmarks-main/autobench-baremetal/benchmarks/logfiles.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python3
-import io
-import os
-if os.path.exists("logfile.txt"):
-  os.remove("logfile.txt")
-logfile = open("logfile.txt",'w+')
-if os.path.exists("errfile.txt"):
-  os.remove("errfile.txt")
-errfile = open("errfile.txt", 'w+')
